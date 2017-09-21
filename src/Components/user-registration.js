c6b9d2f006c83fa6a825c988ae26f356397eb95f
brinadanielle <bri.daniG@gmail.com>
2017-09-21 10:45:20 -0500

    Changed registration to no longer include "profile fields". SDG

--- src/Components/user-registration.js
+++ src/Components/user-registration.js
@@ -1,10 +1,33 @@
 import React from 'react';
-import { Field, reduxForm } from 'redux-form';
+import {connect} from 'react-redux';
+import { Field, reduxForm, formValueSelector} from 'redux-form';
+import isEmail from 'sane-email-validation';
+import fire from '../fire'
+
+const validate = values => {
+ const errors = {}
+ if (!values.firstName){
+   errors.firstName = 'Required'
+ }
+ if (!values.lastName){
+  errors.lastName = 'Required'
+}
+if (!values.email){
+  errors.email = 'Required'
+} else if (isEmail(values.email)){
+  errors.email = 'Invalid email address !'
+}
+  return errors
+};
+
+
 
 let UserForm = props => {
   const { handleSubmit, pristine, reset, submitting } = props
+  const selector = formValueSelector('UserForm')
+
   return (
-    <form onSubmit={ handleSubmit }>
+    <form onSubmit={ props.handleSubmit(doSubmit)}>
     <div>
     <label>User</label>
     </div>
@@ -29,29 +52,21 @@ let UserForm = props => {
         </label>
       </div>
       <div>
-        <label htmlFor="firstName">First Name</label>
-        <Field name="firstName" component="input" type="text" />
+        <label htmlFor="userName" placeholder="Username">Create User Name</label>
+        <Field name="userName" component="input" type="text" />
       </div>
       <div>
-        <label htmlFor="middleName">Middle Name</label>
-        <Field name="middleName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="lastName">Last Name</label>
-        <Field name="lastName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email">E-mail</label>
+        <label htmlFor="email" placeholder="E-mail">E-mail</label>
         <Field name="email" component="input" type="text" />
       </div>
       <div>
-       <label htmlFor="phone">Phone Number</label>
-       <Field name="phone" component="input" type="text" />
-     </div>
-     <div>
-      <label htmlFor="website">Website</label>
-      <Field name="website" component="input" type="text" />
-    </div>
+        <label htmlFor="password" placeholder="password">Create Password</label>
+        <Field name="password" component="input" type="text" />
+      </div>
+      <div>
+        <label htmlFor="confirmPassword" placeholder="password">Confirm Password</label>
+        <Field name="confirmPassword" component="input" type="text" />
+      </div>
     <div>
     <button type="submit" disabled={pristine || submitting}>
       Submit
@@ -59,7 +74,7 @@ let UserForm = props => {
     <button type="button" disabled={pristine || submitting} onClick={reset}>
       Clear Values
     </button>
-  </div>
+    </div>
     </form>
   )
 }
@@ -68,7 +83,8 @@ let UserForm = props => {
 
 UserForm = reduxForm({
   // a unique name for the form
-  form: 'userform'
+  form: 'userform',
+  validate
 })(UserForm);
 
 export {UserForm};
