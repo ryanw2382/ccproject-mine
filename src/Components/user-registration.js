48911f7cd88b79a689c6fb628f5dfccc0cce82c2
brinadanielle <bri.daniG@gmail.com>
2017-09-22 09:39:25 -0500

    totally different form

--- src/Components/user-registration.js
+++ src/Components/user-registration.js
@@ -1,89 +1,83 @@
 import React from 'react';
-import { Field, reduxForm} from 'redux-form';
-import isEmail from 'sane-email-validation';
-
-const validate = values => {
- const errors = {}
- if (!values.userName){
-   errors.userName = 'Required'
- }
- if (!values.password){
-  errors.password = 'Required'
-}
-if (values.password !== values.confirmPassword){
-  errors.password = "Passwords Must Match!"
-}
-if (!values.email){
-  errors.email = 'Required'
-} else if (isEmail(values.email)){
-  errors.email = 'Invalid email address!'
-}
-  return errors
-};
-
-
+import { Field, reduxForm } from 'redux-form';
 
 let UserForm = props => {
-  const {handleSubmit, pristine, reset, submitting } = props
+  const { handleSubmit, pristine, reset, submitting } = props;
   return (
     <form onSubmit={handleSubmit}>
     <div>
     <label>User</label>
-    </div>
     <div>
       <label>
-        <Field
-          name="user"
-          component="input"
-          type="radio"
-          value="job-candidate"
-          />{' '}
-          Job-Candidate
-       </label>
-       <label>
-        <Field
-          name="user"
-          component="input"
-          type="radio"
-          value="employer"
-          />{' '}
-          Employer
-        </label>
-      </div>
-      <div>
-        <label htmlFor="userName" placeholder="Username">Create User Name</label>
-        <Field name="userName" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="email" placeholder="E-mail">E-mail</label>
-        <Field name="email" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="password" placeholder="password">Create Password</label>
-        <Field name="password" component="input" type="text" />
-      </div>
-      <div>
-        <label htmlFor="confirmPassword" placeholder="password">Confirm Password</label>
-        <Field name="confirmPassword" component="input" type="text" />
-      </div>
-    <div>
-    <button type="submit" disabled={pristine || submitting}>
-      Submit
-    </button>
-    <button type="button" disabled={pristine || submitting} onClick={reset}>
-      Clear Values
-    </button>
+        <Field name="user" component="input" type="radio" value="Job-Candidate"/>
+        {' '}
+        Job-Candidate
+      </label>
+      <label>
+        <Field name="user" component="input" type="radio" value="employer" />
+        {' '}
+        Employer
+      </label>
     </div>
+  </div>
+      <div>
+        <label>Username</label>
+        <div>
+          <Field
+            name="userName"
+            component="input"
+            type="text"
+            placeholder="User Name"
+          />
+        </div>
+      </div>
+      <div>
+        <label>Email</label>
+        <div>
+          <Field
+            name="email"
+            component="input"
+            type="email"
+            placeholder="Email"
+          />
+        </div>
+      </div>
+      <div>
+      <label>Create Password</label>
+      <div>
+        <Field
+          name="createPassword"
+          component="input"
+          type="password"
+          placeholder="Password"
+        />
+      </div>
+    </div>
+    <div>
+    <label>Confirm Password</label>
+    <div>
+      <Field
+        name="confirmPassword"
+        component="input"
+        type="password"
+        placeholder="Password"
+      />
+    </div>
+  </div>
+  <div>{ {"user":"employer"}.checked && <span>Thank you, Jesus!</span> }</div>
+      <div>
+        <button type="submit" disabled={pristine || submitting}>Submit</button>
+        <button type="button" disabled={pristine || submitting} onClick={reset}>
+          Clear Values
+        </button>
+      </div>
     </form>
-  )
-}
-
-
+  );
+};
 
 UserForm = reduxForm({
   // a unique name for the form
   form: 'userform',
-  validate
 })(UserForm);
 
 export {UserForm};
