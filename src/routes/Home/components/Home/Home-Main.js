360e1c231f4f9bbb65b1edc5defa907a1bbc5bd5
brinadanielle <bri.daniG@gmail.com>
2017-10-04 03:52:11 -0500

    added stuff...and working slider before cards and parallax

---
+++ src/routes/Home/components/Home/Home-Main.js
@@ -0,0 +1,32 @@
+import React from 'react';
+import {Slider, Slide} from 'react-materialize';
+import comp_coff from "../../../../img/comp_coff.jpg";
+import tech from "../../assets/tech.png";
+import hero from "../../assets/bg-home-technologies.jpg";
+
+// import { BrowserRouter as NavLink } from "react-router-dom";
+
+export const HomeMain = (props) => {
+  return (
+    <Slider>
+	 <Slide
+		src={comp_coff}
+        title="In Need of a Web Developer ?">
+		Log-In to hire a developer for your next project.
+	</Slide>
+	<Slide
+		src={tech}
+		title="Fullstack Developers"
+		placement="left">
+		Equipped with the latest technologies. 
+	</Slide>
+	<Slide
+		src={hero}
+		title="Our Developers Are..."
+		placement="right">
+		A great addition to any team.
+	</Slide>
+</Slider>
+)};
+
+export default HomeMain;
