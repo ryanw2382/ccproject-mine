147676144317c76fdb7f421fe2fafa1357da1240
drewpete4 <drew.pete4@gmail.com>
2017-10-04 08:23:37 -0700

    employer dashboard changes

--- src/routes/Projects/components/ProjectTile/ProjectTile.js
+++ src/routes/Projects/components/ProjectTile/ProjectTile.js
@@ -1,37 +1,45 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import Paper from 'material-ui/Paper';
-import { isObject } from 'lodash';
-import IconButton from 'material-ui/IconButton';
-import DeleteIcon from 'material-ui/svg-icons/action/delete';
-import classes from './ProjectTile.scss';
-
+import React from "react";
+import PropTypes from "prop-types";
+import Paper from "material-ui/Paper";
+import { isObject } from "lodash";
+import IconButton from "material-ui/IconButton";
+import DeleteIcon from "material-ui/svg-icons/action/delete";
+import classes from "./ProjectTile.scss";
 
 export const ProjectTile = ({ project, onSelect, onDelete, showDelete }) => (
-    <Paper className={classes.container}>
-        <div className={classes.top}>
-      <span className={classes.name} onClick={() => onSelect(project)}>
-        {project.name}
-      </span>
-            {showDelete && onDelete ? (
-                <IconButton tooltip="delete" onClick={onDelete}>
-                    <DeleteIcon />
-                </IconButton>
-            ) : null}
+  <Paper className={classes.container}>
+    <div className={classes.top}>
+      <div>
+        <span className={classes.jobName} onClick={() => onSelect(project)}>
+          <div className={classes.flexItem}>{project.jobName}</div>
+        </span>
+        <div className={classes.flexItem}>
+          <b>Salary: </b>
+          {project.salary}
         </div>
-        <span className={classes.owner}>
-        {isObject(project.createdBy) ? (
-            project.createdBy.displayName
-        ) : (
-            project.createdBy || 'No Owner'
-        )}
+        <div className={classes.flexItem}>{project.preferredSkills}</div>
+        <div className={classes.flexItem}>{project.devSelect}</div>
+      </div>
+
+      {showDelete && onDelete ? (
+        <IconButton tooltip="delete" onClick={onDelete}>
+          <DeleteIcon />
+        </IconButton>
+      ) : null}
+    </div>
+    <span className={classes.owner}>
+      {isObject(project.createdBy)
+        ? project.createdBy.displayName
+        : project.createdBy || "No Owner"}
     </span>
-    </Paper>
+  </Paper>
 );
+
 ProjectTile.propTypes = {
-    project: PropTypes.object.isRequired,
-    onSelect: PropTypes.func.isRequired,
-    onDelete: PropTypes.func,
-    showDelete: PropTypes.bool
+  project: PropTypes.object.isRequired,
+  onSelect: PropTypes.func.isRequired,
+  onDelete: PropTypes.func,
+  showDelete: PropTypes.bool
 };
+
 export default ProjectTile;
