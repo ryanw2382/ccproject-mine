ab39b48bf50ee8c6fb9e3ef85ddf65fe881102af
Ryan Wilson <ryanw2382@gmail.com>
2017-10-04 20:27:29 -0700

    part of the process of cleaning up the routes

--- src/routes/Login/index.js
+++ src/routes/Login/index.js
@@ -1,17 +1,7 @@
-import { LOGIN_PATH as path } from './../../constants'
-export default (store) => ({
-    path,
-    /*  Async getComponent is only invoked when route matches   */
-    getComponent (nextState, cb) {
-        /*  Webpack - use 'require.ensure' to create a split point
-            and embed an async module loader (jsonp) when bundling   */
-        require.ensure([], (require) => {
-            /*  Webpack - use require callback to define
-                dependencies for bundling   */
-            const Login = require('./containers/LoginContainer').default;
-            /*  Return getComponent   */
-            cb(null, Login)
-            /* Webpack named bundle   */
-        }, 'Login')
-    }
-})
+import { LOGIN_PATH as path } from '../../constants'
+import component from './containers/LoginContainer'
+
+export default {
+  path,
+  component
+}
